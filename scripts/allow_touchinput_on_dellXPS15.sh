d0326e58ee8e312c4f89ef7f380e821e053b9aa9
gr4viton <lordmutty@gmail.com>
2017-05-27 02:31:46 +0200

    added kivy config.ini details for touchscreen functionality

--- scripts/allow_touchinput_on_dellXPS15.sh
+++ scripts/allow_touchinput_on_dellXPS15.sh
@@ -1,2 +1,10 @@
 sudo chmod 666 /dev/input/event10
 sudo chmod 666 /dev/input/event19
+
+
+echo "you must manually add these lines under section [input] into the config file `~/.kivy/config.init`"
+echo "mtdev_%(name)s = probesysfs,provider=mtdev"
+echo "hid_%(name)s = probesysfs,provider=hidinput"
+echo "touch = probesysfs,match=E3 Finger,provider=linuxwacom,
+    select_all=1,param=mode=touch"
+
